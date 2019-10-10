142913a46665b77af62c8d834d5c3a9dd212580e
BenBonson <bonsonben@gmail.com>
2019-10-10 11:35:18 -0500

    So many ifs

--- main.go
+++ main.go
@@ -38,28 +38,43 @@ func main() {
 	var name string
 	fmt.Println("Hello adventurer please state your name.")
 	fmt.Scanln(&name)
+	//add locations to go, shop arena bank
+	var tolocation string
+	fmt.Println("Thanks for playing my game, where would you like to go?. \n [S]hop [A]rena [B]ank")
+	fmt.Scanln(&tolocation)
+	var tolocationlower = strings.ToLower(tolocation)
+	if tolocationlower == "s" {
+		fmt.Println("Sorry not avalible yet")
+	} else if tolocationlower == "a" {
+		fmt.Println("Sorry not avalible yet")
+	} else if tolocationlower == "b" {
+		fmt.Println("sup")
+		//location = bank or something would probably be cleaner
+		//check for bank account
+		if bank.Account() {
+			fmt.Println("Welcome back", name, "how can I help you?") //add options deposit withdraw
+		} else {
+			fmt.Println("Welcome", name, "I see you do not have an account with us would you like to set one up? [Y] or [N]")
+			//create account
+			var acreate string
+			fmt.Scanln(&acreate)
+			var acreatelower = strings.ToLower(acreate)
+			//check for user input
+			//fmt.Println(acreatelower)
+			if acreatelower == "y" {
+				fmt.Println("Alright let me create that for you. Just so you know if you store your gold at the bank you will be unable to bet it however you wont lose it if you are beaten.")
+				//add new account with a 0 balance under the player name
+				db.Exec("insert into bankaccount (name, funds) values ($1, 0)", name)
+				//check table
+				//getAll(db)
+			} else {
+				fmt.Println("Well, good luck with that. Just so you know if you store your gold at the bank you will be unable to bet it however you wont lose it if you are beaten.")
+			}
 
-	//check for bank account
-	if bank.Account() {
-		fmt.Println("Welcome back", name, "how can I help you?") //add options deposit withdraw
+		}
 	} else {
-		fmt.Println("Welcome", name, "I see you do not have an account with us would you like to set one up? [Y] or [N]")
-	}
-
-	//create account
-	var acreate string
-	fmt.Scanln(&acreate)
-	var acreatelower = strings.ToLower(acreate)
-	//check for user input
-	//fmt.Println(acreatelower)
-	if acreatelower == "y" {
-		fmt.Println("Alright let me create that for you. Just so you know if you store your gold at the bank you will be unable to bet it however you wont lose it if you are beaten.")
-		//add new account with a 0 balance under the player name
-		db.Exec("insert into bankaccount (name, funds) values ($1, 0)", name)
-		//check table
-		//getAll(db)
-	} else {
-		fmt.Println("Well, good luck with that. Just so you know if you store your gold at the bank you will be unable to bet it however you wont lose it if you are beaten.")
+		fmt.Println("Sorry not an option please choose S]hop [A]rena [B]ank")
+		//figure out how to reask for input
 	}
 
 	//Notes and references
